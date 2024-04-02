404135cf9b6f2f71ed8e43b7032b684aa18a61e1
jormunggand <theo.atakpama@ensta-paris.fr>
2024-04-02 15:04:54 +0200

    test de SDL_image

--- code/test.c
+++ code/test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 
 int main() {
 	printf("BOMBERMAN\n");
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		printf("Erreur\n");
 	return 0;
 }
