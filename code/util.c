fdef9bb049383d2b451a96f86e979196fe8136f0
Arthur <arthur.foucard@ensta-paris.fr>
2024-04-09 16:15:06 +0200

    utli file

--- code/util.c
+++ code/util.c
@@ -1,7 +1,46 @@
-#include "util.c"
+#include "util.h"
 
-SDL_Texture load_image(char* name, ) {
-    SDL_Surface* tmp = IMG_Load(name);
-    if (tmp == NULL)
-        printf("Error while loading file: %s", IMG_GetError)
+
+int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h)
+{
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+        return -1;
+    }
+    if(0 != SDL_CreateWindowAndRenderer(w, h, SDL_WINDOW_SHOWN, window, renderer))
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindowAndRenderer : %s", SDL_GetError());
+        return -1;
+    }
+    return 0;
+}
+
+SDL_Texture* loadImage(const char path[], SDL_Renderer *renderer)
+{
+    SDL_Surface *tmp = NULL; 
+    SDL_Texture *texture = NULL;
+    tmp = IMG_Load(path);
+    if(NULL == tmp)
+    {
+        fprintf(stderr, "Erreur IMG_Load : %s", SDL_GetError());
+        return NULL;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, tmp);
+    SDL_FreeSurface(tmp);
+    if(NULL == texture)
+    {
+        fprintf(stderr, "Erreur SDL_CreateTextureFromSurface : %s", SDL_GetError());
+        return NULL;
+    }
+    return texture;
+}
+
+int setWindowColor(SDL_Renderer *renderer, SDL_Color color)
+{
+    if (SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a) < 0)
+        return -1;
+    if (SDL_RenderClear(renderer) < 0)
+        return -1;
+    return 0;  
 }
--- code/util.h
+++ code/util.h
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#ifdef __linux__
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#elif __APPLE__
+#include <SDL.h>
+#include <SDL_image.h>
+#endif
 
-SDL_Texture load_image(char* name, SDL_Renderer** render);
+SDL_Texture* load_image(char* name, SDL_Renderer** render);
+int setWindowColor(SDL_Renderer *renderer, SDL_Color color);
+int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
