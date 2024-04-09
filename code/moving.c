da76791a41b6e3d5bb9b34959f93fee3935bd591
mike <mike@tyson>
2024-04-09 16:39:40 +0200

    réorga

--- code/moving.c
+++ code/moving.c
@@ -3,6 +3,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+#include "utils.h"
+
 int main(int argc, char* argv[]) {
     int exit_status = EXIT_FAILURE;
     if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0) {
@@ -18,12 +20,16 @@ int main(int argc, char* argv[]) {
     }
     SDL_SetWindowTitle(window, "Moving Bomberman");
 
-    
+    SDL_Texture* player = loadImage("../assets/bomberman-sprite.png", render);
+    SDL_Rect dst = {320,320,0,0};
+    SDL_QueryTexture(player, 0, 0, &dst.w, &dst.h);
 
-    
-
+    SDL_RenderCopy(render, player, NULL, &dst);
     SDL_RenderPresent(render);
 
+    SDL_Delay(3000);
+
+    SDL_DestroyTexture(player);
     SDL_DestroyRenderer(render);
     SDL_DestroyWindow(window);
     exit_status = EXIT_SUCCESS;
