76d531351e5e48c35038cc08f13e9265be2aa657
Arthur <arthur.foucard@ensta-paris.fr>
2024-04-12 21:19:38 +0200

    added some map gestion features

--- code/map.c
+++ code/map.c
@@ -1,65 +1,40 @@
 #include "utils.h"
 
-
+// the size of the board is currently fixed by defines but this will maybe be changed in the future to have variable sizes
 #define MAP_SIZE (16)
-#define TILE_SIZE (64 / 2)
+#define TILE_SIZE (32)
 
-int main(int argc, char* argv[]) {
-    int exit_status = EXIT_FAILURE;
-    char* wall_tile = "wall_tile.png";
-    char* floor_tile = "normal_tile.png";
+typedef enum { // Tile types, will add the other types later
+    HARD_WALL,
+    EMPTY,
+    SOFT_WALL
+} TileType;
 
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
-    SDL_Texture* floor_texture = NULL; 
-    SDL_Texture* wall_texture = NULL;
 
-    if (init(&window, &renderer, MAP_SIZE * TILE_SIZE, MAP_SIZE * TILE_SIZE) != 0) {
-        printf("%s\n", SDL_GetError());
-        goto Quit;
+// function to load the map from a text file
+void read_map_from_file(int** map, char* file_name) {
+    FILE* file = fopen(file_name, "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return;
     }
-    SDL_SetWindowTitle(window, "Map");
-
-    floor_texture = loadImage(floor_tile, renderer);
-    wall_texture = loadImage(wall_tile, renderer);
-    if (floor_texture == NULL || wall_texture == NULL) {
-        printf("%s", SDL_GetError());
-        goto Quit;
+    for (int i = 0; i < MAP_SIZE; i++) {
+        for (int j = 0; j < MAP_SIZE; j++) {
+            map[i][j] = fgetc(file) - '0';
+        }
+        fgetc(file); // to skip the newline character
     }
+    fclose(file);
+}
 
+// a function to update the renderer given the current state of the map
+void update_renderer(SDL_Renderer* renderer, int** map, SDL_Texture* textures[], int nb_textures) {
     SDL_Rect rect = {0, 0, TILE_SIZE, TILE_SIZE};
-
     for (int i = 0; i < MAP_SIZE; i++) {
         for (int j = 0; j < MAP_SIZE; j++) {
             rect.x = i * TILE_SIZE;
             rect.y = j * TILE_SIZE;
-            if (i == 0 || j == 0 || i == MAP_SIZE - 1 || j == MAP_SIZE - 1) {
-                SDL_RenderCopy(renderer, wall_texture, NULL, &rect);
-            } else {
-                SDL_RenderCopy(renderer, floor_texture, NULL, &rect);
-            }
+            SDL_RenderCopy(renderer, textures[map[i][j]], NULL, &rect);
         }
     }
-    SDL_RenderPresent(renderer);
-    bool is_running = true;
-    SDL_Event event;
-    while (is_running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                is_running = false;
-            }
-        }
-        SDL_Delay(16);
-    }
-    Quit:
-    if (NULL != wall_texture)
-        SDL_DestroyTexture(wall_texture);
-    if (NULL != floor_texture)
-        SDL_DestroyTexture(floor_texture);
-    if(NULL != renderer)
-        SDL_DestroyRenderer(renderer);
-    if(NULL != window)
-        SDL_DestroyWindow(window);
-    SDL_Quit();
-    return exit_status;
 }
---
+++ code/map_test.c
@@ -0,0 +1,66 @@
+#include "utils.h"
+
+#define MAP_SIZE (16)
+#define TILE_SIZE (32)
+
+int main(int argc, char* argv[]) {
+    int** map = malloc(MAP_SIZE * sizeof(int*));
+    for (int i = 0; i < MAP_SIZE; i++) {
+        map[i] = malloc(MAP_SIZE * sizeof(int));
+    }
+    int exit_status = EXIT_FAILURE;
+    char* wall_tile = "wall_tile.png";
+    char* empty_tile = "normal_tile.png";
+    char* soft_wall_tile = "soft_wall_tile.png";
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Texture* empty_texture = NULL; 
+    SDL_Texture* wall_texture = NULL;
+    SDL_Texture* soft_wall_texture = NULL;
+
+    if (init(&window, &renderer, MAP_SIZE * TILE_SIZE, MAP_SIZE * TILE_SIZE) != 0) {
+        printf("%s\n", SDL_GetError());
+        goto Quit;
+    }
+    SDL_SetWindowTitle(window, "Map");
+
+    empty_texture = loadImage(empty_tile, renderer);
+    wall_texture = loadImage(wall_tile, renderer);
+    soft_wall_texture = loadImage(soft_wall_tile, renderer);
+    if (empty_texture == NULL || wall_texture == NULL || soft_wall_texture == NULL) {
+        printf("%s", SDL_GetError());
+        goto Quit;
+    }
+
+    SDL_Texture* textures[] = {wall_texture, empty_texture, soft_wall_texture};
+    read_map_from_file(map, "map_example.txt");
+    printf("%d\n", map[2][2]);
+    update_renderer(renderer, map, textures, 3);
+
+    SDL_RenderPresent(renderer);
+    bool is_running = true;
+    SDL_Event event;
+    while (is_running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                is_running = false;
+            }
+        }
+        SDL_Delay(16);
+    }
+    Quit:
+    if (NULL != wall_texture)
+        SDL_DestroyTexture(wall_texture);
+    if (NULL != empty_texture)
+        SDL_DestroyTexture(empty_texture);
+    if(NULL != renderer)
+        SDL_DestroyRenderer(renderer);
+    if(NULL != window)
+        SDL_DestroyWindow(window);
+    SDL_Quit();
+    for (int i = 0; i < MAP_SIZE; i++) {
+        free(map[i]);
+    }
+    free(map);
+    return exit_status;
+}
