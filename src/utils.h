7473f4033b7d525f71ccfcf2832d963e1c883548
mike <mike@tyson>
2024-04-14 17:55:27 +0200

    global constants nd structs will now be stored in utils.h

--- src/utils.h
+++ src/utils.h
@@ -10,6 +10,18 @@
 #include <SDL_image.h>
 #endif
 
+// might be changed in the future
+#define N_TEXTURES (5)
+// the size of the board is currently fixed by defines but this will maybe be changed in the future to have variable sizes
+#define MAP_SIZE (16)
+#define TILE_SIZE (64)
+
+typedef enum { // Tile types, will add the other types later
+    HARD_WALL,
+    EMPTY,
+    SOFT_WALL
+} TileType;
+
 int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
 SDL_Texture* loadImage(const char path[], SDL_Renderer *renderer);
 int setColor(SDL_Renderer *renderer, SDL_Color color);
