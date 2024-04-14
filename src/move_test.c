df565325ec8bffd3b2a78b7fe6b4b67bf6c0bd3f
mike <mike@tyson>
2024-04-14 18:57:20 +0200

    minor changes

--- src/move_test.c
+++ src/move_test.c
@@ -2,64 +2,52 @@
 #include "map.h"
 #include "move.h"
 
-// gère la collision du rectangle avec les bords de la fenêtre
-/*void edge_collision(SDL_Window* window, SDL_Rect* rect) {
-    int width, height;
-    SDL_GetWindowSize(window, &width, &height);
-    if (rect->x < 0)
-        rect->x = 0;
-    if (rect->y < 0)
-        rect->y = 0;
-    if (rect->x + rect->w > width)
-        rect->x = width - rect->w;
-    if (rect->y + rect->h > height)
-        rect->y = height - rect->h;
-}*/
-
 int main(int argc, char* argv[]) {
     int exit_status = EXIT_FAILURE;
 
     SDL_Window* window = NULL;
     SDL_Renderer* render = NULL;
-    if (0 != init(&window, &render, MAP_SIZE*TILE_SIZE, MAP_SIZE*TILE_SIZE)) {
+    int windowWidth = MAP_SIZE*TILE_SIZE, windowHeight = MAP_SIZE*TILE_SIZE;
+    if (0 != init(&window, &render, windowWidth, windowHeight)) {
         printf("%s", SDL_GetError());
         goto Quit;
     }
 
     // load and display map
-    int** map = read_map_from_file("map_collision.txt", MAP_SIZE, MAP_SIZE);
+    int** map = read_map_from_file("map_example.txt", MAP_SIZE, MAP_SIZE);
     display_map(render, map, MAP_SIZE, MAP_SIZE);
 
     // load and display player sprite
     SDL_Texture* player = loadImage("../assets/Bomberman/Front/Bman_F_f00.png", render);
-    SDL_Rect dst = {.x = MAP_SIZE/2, .y = MAP_SIZE/2, .w = 32, .h = 32};
-    SDL_QueryTexture(player, 0, 0, &dst.w, &dst.h);
-    SDL_RenderCopy(render, player, NULL, &dst);
+    SDL_Rect playerRect = {.x = windowWidth/2, .y = windowHeight/2, .w = 32, .h = 32};
+    SDL_QueryTexture(player, 0, 0, &playerRect.w, &playerRect.h);
+    SDL_RenderCopy(render, player, NULL, &playerRect);
     
     SDL_RenderPresent(render);
     
 
-    int velocity = 58;
+    int velocity = 40;
     SDL_Event event;
     bool done = false;
     while (!done) {
         SDL_WaitEvent(&event);
-        if (event.type == SDL_QUIT)
+        if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_q))
             done = true;
         if (event.type == SDL_KEYDOWN) {
             if (event.key.keysym.sym == SDLK_DOWN)
-                dst.y += velocity;
+                playerRect.y += velocity;
             else if (event.key.keysym.sym == SDLK_UP)
-                dst.y -= velocity;
+                playerRect.y -= velocity;
             else if (event.key.keysym.sym == SDLK_RIGHT)
-                dst.x += velocity;
+                playerRect.x += velocity;
             else if (event.key.keysym.sym == SDLK_LEFT)
-                dst.x -= velocity;
+                playerRect.x -= velocity;
 
-            edge_collision(window, &dst, map);
+            edge_collision(window, &playerRect, map);
+
             SDL_RenderClear(render);
             display_map(render, map, MAP_SIZE, MAP_SIZE);
-            SDL_RenderCopy(render, player, NULL, &dst);
+            SDL_RenderCopy(render, player, NULL, &playerRect);
             SDL_RenderPresent(render);
         }
     }
