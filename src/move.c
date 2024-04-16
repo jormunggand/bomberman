ebf84adecebb63d8f696e92afcf73b70521cfbdb
Arthur <arthur.foucard@ensta-paris.fr>
2024-04-16 17:42:58 +0200

    improved collision handling
    The player rectangle was not centered properly on the y-axis so the collision rectangle's position must be offset by TILE_SIZE / 4

--- src/move.c
+++ src/move.c
@@ -2,8 +2,10 @@
 #include "move.h"
 #include "utils.h"
 
-float eps = TILE_SIZE / 4;
+float eps = 0.5 + TILE_SIZE / 4; // tolerance for collision detection
 
+// moves the player with the veolicitites velx and vely if there is no collision that forbids it
+// the collisions are calculated with collision_rect
 void edge_collision(SDL_Window* window, SDL_Rect* player_rect, SDL_Rect* collision_rect, int** map, int velx, int vely) {
     int width, height;
     SDL_GetWindowSize(window, &width, &height);
@@ -16,8 +18,8 @@ void edge_collision(SDL_Window* window, SDL_Rect* player_rect, SDL_Rect* collisi
         }
     }
     if (vely != 0){
-        collision_rect->y += vely;
         player_rect->y += vely;
+        collision_rect->y += vely;
         if (collision_rect->y < 0 || collision_rect->y + collision_rect->h > height || check_collision(collision_rect, map)){
             collision_rect->y -= vely;
             player_rect->y -= vely;
@@ -25,6 +27,7 @@ void edge_collision(SDL_Window* window, SDL_Rect* player_rect, SDL_Rect* collisi
     }
 }
 
+// returns true if there is a collision between the rect and a wall
 bool check_collision(SDL_Rect* r, int** map){
     for (int i = 0; i < 2; i++){
         for (int j = 0; j < 2; j++){
