7b2c78ece21b09231c631f6b581eed99ee8c2c34
mike <mike@tyson>
2024-04-17 21:00:27 +0200

    added new enum for the bonuses

--- src/utils.h
+++ src/utils.h
@@ -11,20 +11,21 @@
 #endif
 
 // might be changed in the future
-#define N_TEXTURES (5)
+#define N_TEXTURES (10)
 // the size of the board is currently fixed by defines but this will maybe be changed in the future to have variable sizes
 #define TILE_SIZE (64)
 
 typedef enum { // Tile types, will add the other types later
-    HARD_WALL,
-    EMPTY,
-    SOFT_WALL,
-    BOMB_BONUS_HIDDEN,
-    BOMB_BONUS,
-    FLAME_BONUS_HIDDEN,
-    FLAME_BONUS,
-    SPEED_BONUS_HIDDEN,
-    SPEED_BONUS
+    HARD_WALL = 0,
+    EMPTY = 1,
+    SOFT_WALL = 2,
+    PLAYER_POS = 3,
+    BOMB_BONUS_HIDDEN = 4,
+    BOMB_BONUS = 5,
+    FLAME_BONUS_HIDDEN = 6,
+    FLAME_BONUS = 7,
+    SPEED_BONUS_HIDDEN = 8,
+    SPEED_BONUS = 9
 } TileType;
 
 int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
