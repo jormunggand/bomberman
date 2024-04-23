a8f226b49c36f006cfc7232822b06900114e54f3
mike <mike@tyson>
2024-04-23 13:08:28 +0200

    get_bonus now handle new map struct

--- src/move.c
+++ src/move.c
@@ -143,18 +143,18 @@ void get_bonus(Player* player, struct Map* map) {
         for (int j = 0; j < 2; j++){
             int x = (r->x + i * (r->w - eps) + (1 - i) * eps) / TILE_SIZE;
             int y = (r->y + j * (r->h - eps)  + (1 - j) * eps) / TILE_SIZE;
-            switch (map->grid[y][x]) {
+            switch (map->grid[y][x].type) {
                 case BOMB_BONUS:
                     player->nBombs = min(MAX_BOMBS, player->nBombs+1);
-                    map->grid[y][x] = EMPTY;
+                    map->grid[y][x].type = EMPTY;
                     break;
                 case FLAME_BONUS:
                     player->flamePower = min(MAX_POWER, player->flamePower+1);
-                    map->grid[y][x] = EMPTY;
+                    map->grid[y][x].type = EMPTY;
                     break;
                 case SPEED_BONUS:
                     player->speed = min(MAX_SPEED, player->speed + SPEED_BOOST);
-                    map->grid[y][x] = EMPTY;
+                    map->grid[y][x].type = EMPTY;
                     break;
             }
         }
