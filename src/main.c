50dec64f1be031ed5d4b469cefa0e6b3abf62c34
mike <mike@tyson>
2024-04-23 14:19:30 +0200

    added support for keyboard handler

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "keyboard.h"
 
 #ifndef MAP_H
 #define MAP_H
@@ -55,52 +56,63 @@ int main(int argc, char* argv[]) {
     
 
     SDL_Event event;
+    KeyboardHandler handler;
+    initHandler(&handler);
+
     bool done = false;
     int cpt_reset = 0, vx = 0, vy = 0;
 
     while (!done) {
         int eventPresent = SDL_PollEvent(&event);
         if (eventPresent) {
-            player.isWalking = true;
-            if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
+            if (event.type == SDL_QUIT || handler.keyState[K_ESC] == SDL_PRESSED)
                 done = true;
-            else if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_DOWN || event.key.keysym.sym == SDLK_s) {
-                    vy += player.speed;
-                    change_direction(&player, FRONT);
-                } else if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_z) {
-                    vy -= player.speed;
-                    change_direction(&player, BACK);
-                } else if (event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d) {
-                    vx += player.speed;
-                    change_direction(&player, RIGHT);
-                } else if (event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_q) {
-                    vx -= player.speed;
-                    change_direction(&player, LEFT);
-                }
-                else{
-                    cpt_reset++;
-                    if (cpt_reset == 200){
-                        cpt_reset = 0;
-                        player.isWalking = false;
-                    }
-                }
-                if (event.key.keysym.sym == SDLK_SPACE) {
-                    player_place_bomb(&player, &map);
-                }
-                update_sprite(&player);
-                edge_collision(window, &player, &map, vx, vy);
-                get_bonus(&player, &map);
-                //printf("%d %d %d\n", player.nBombs, player.flamePower, player.speed);
-                vx = 0; vy = 0;
+            else if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+                handleEvent(&handler, event.key);
             }
-            if (event.type == SDL_MOUSEBUTTONDOWN) {
+            else if (event.type == SDL_MOUSEBUTTONDOWN) {
                 int x, y;
                 SDL_GetMouseState(&x, &y);
                 int i = x / TILE_SIZE, j = y / TILE_SIZE;
                 add_bomb(&map, i, j);
             }
         }
+
+        if (handler.keyState[K_DOWN] == SDL_PRESSED) {
+            player.isWalking = true;
+            vy += player.speed;
+            change_direction(&player, FRONT);
+        } else if (handler.keyState[K_UP] == SDL_PRESSED) {
+            player.isWalking = true;
+            vy -= player.speed;
+            change_direction(&player, BACK);
+        } else if (handler.keyState[K_RIGHT] == SDL_PRESSED) {
+            player.isWalking = true;
+            vx += player.speed;
+            change_direction(&player, RIGHT);
+        } else if (handler.keyState[K_LEFT] == SDL_PRESSED) {
+            player.isWalking = true;
+            vx -= player.speed;
+            change_direction(&player, LEFT);
+        }
+        else{
+            cpt_reset++;
+            if (cpt_reset == 200){
+                cpt_reset = 0;
+                player.isWalking = false;
+            }
+        }
+
+        if (handler.keyState[K_SPACE] == SDL_PRESSED) {
+            player_place_bomb(&player, &map);
+        }
+
+        update_sprite(&player);
+        edge_collision(window, &player, &map, vx, vy);
+        get_bonus(&player, &map);
+        //printf("%d %d %d\n", player.nBombs, player.flamePower, player.speed);
+        vx = 0; vy = 0;
+
         SDL_RenderClear(render);
         display_map(render, &map);
         display_bombs(render, &map);
@@ -108,6 +120,8 @@ int main(int argc, char* argv[]) {
         //SDL_RenderDrawRect(render, &player.rect);
         //SDL_RenderDrawRect(render, &player.collisionRect);
         SDL_RenderPresent(render);
+
+        SDL_Delay(32);
     }
 
 
