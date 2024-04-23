42edab1461f1ee66c459495dd53c595163ab7ecb
mike <mike@tyson>
2024-04-23 13:40:46 +0200

    added powerup constants here

--- src/utils.h
+++ src/utils.h
@@ -15,18 +15,13 @@
 // the size of the board is currently fixed by defines but this will maybe be changed in the future to have variable sizes
 #define TILE_SIZE (64)
 
-typedef enum { // Tile types, will add the other types later
-    HARD_WALL = 0,
-    EMPTY = 1,
-    SOFT_WALL = 2,
-    PLAYER_POS = 3,
-    BOMB_BONUS_HIDDEN = 4,
-    BOMB_BONUS = 5,
-    FLAME_BONUS_HIDDEN = 6,
-    FLAME_BONUS = 7,
-    SPEED_BONUS_HIDDEN = 8,
-    SPEED_BONUS = 9
-} TileType;
+// Powerups constants
+#define BASE_SPEED (16) // in pixels
+#define SPEED_BOOST (4) // in pixels
+#define MAX_SPEED (28)
+
+#define MAX_BOMBS (5)
+#define MAX_POWER (5) // in number of tiles
 
 int min(int a, int b);
 int max(int a, int b);
