41ab6a3f77edadf64dbff8987cfe493087ca9822
mike <mike@tyson>
2024-05-14 16:10:13 +0200

    waiting screen

--- src/online.c
+++ src/online.c
@@ -7,8 +7,13 @@ void host_server(SDL_Renderer* render, int windowWidth, int windowHeight) {
     SDLNet_ResolveHost(&ip, NULL, 1234); // listen on port 1234
     serverSocket = SDLNet_TCP_Open(&ip);
 
+    // Draw waiting screen
     SDL_SetRenderDrawColor(render, 37, 150,190, 255);
     SDL_RenderClear(render);
+
+    TTF_Font* nasa = TTF_OpenFont("../assets/Fonts/nasa.ttf", 24);
+
+    SDL_RenderPresent(render);
 
     do {
         clientScoket = SDLNet_TCP_Accept(serverSocket);
